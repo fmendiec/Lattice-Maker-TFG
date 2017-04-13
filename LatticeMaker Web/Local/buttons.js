dbde21eb92ac64db70fccd94e4fb64a9d3f9cca4
Felix_MC <femenca9@gmail.com>
2017-04-13 19:18:35 +0200

    Fixed regexp for looking an arc when remove

--- LatticeMaker Web/Local/buttons.js
+++ LatticeMaker Web/Local/buttons.js
@@ -145,7 +145,51 @@ var detectSelectedMember = function(canvas,event)
     return chosen;
 }
 
-var manageArc = function(act,idCanvas,idTextArea)
+var createArc = function(idCanvas,idTextArea)
+{
+    var addArc = function(member1,member2)
+    {
+        // Load TextArea
+        var textarea = document.getElementById(idTextArea);
+        
+        // Get the index of the last member
+        var code = $("#"+idTextArea).val();
+        var regex_arc = /arc\s*\(\s*([^,()]+\s*,\s*[^,()]+)\s*\)\s*\./g;
+        var match = true;
+        var index = 0;
+        while(match = regex_arc.exec(code)){index = match.index + match[0].length};
+    
+        // Append the new member to TextArea
+        textarea.value = [textarea.value.slice(0,index), 'arc('+member1+','+member2+').', textarea.value.slice(index+1)].join('\n');
+        
+        // Updating TextArea by triggering the event "change"
+        $("#"+idTextArea).trigger('change');
+    }
+    
+    selectTwo(idCanvas,addArc);   
+}
+
+var removeArc = function(idCanvas,idTextArea)
+{   
+    var deleteArc = function(member1,member2)
+    {
+        // Load TextArea
+        var textarea = document.getElementById(idTextArea);
+        
+        // Delete arcs, top and bottom predicates
+        var code = $("#"+idTextArea).val();
+        var regex_arc = new RegExp("arc\\s*\\(\\s*(("+member1+"\\s*,\\s*"+member2+")\|("+member2+"\\s*,\\s*"+member1+"))\\s*\\)\\s*\\.","g");
+        code = code.replace(regex_arc,'\r');
+        
+        // Update Text and Graph
+        textarea.value = code;
+        $("#"+idTextArea).trigger('change');
+    }
+    
+    selectTwo(idCanvas,deleteArc);
+}
+
+var selectTwo = function(idCanvas, func)
 {
     // Load Canvas
     var canvas = document.getElementById(idCanvas);
@@ -167,12 +211,7 @@ var manageArc = function(act,idCanvas,idTextArea)
                 
                 // Member selected
                 if (select1 != null && select2 != null)
-                {
-                    if (act == 0)
-                        addArc(select1.member,select2.member,idTextArea);
-                    if (act == 1)
-                        deleteArc(select1.member,select2.member,idTextArea);
-                }
+                    func(select1.member,select2.member);
 
                 if (select1 == null)
                     alert('Member 1 not selected')
@@ -182,39 +221,4 @@ var manageArc = function(act,idCanvas,idTextArea)
                     
             });
     });
-    
-    var addArc = function(member1,member2,idTextArea)
-    {
-        // Load TextArea
-        var textarea = document.getElementById(idTextArea);
-        
-        // Get the index of the last member
-        var code = $("#"+idTextArea).val();
-        var regex_arc = /arc\s*\(\s*([^,()]+\s*,\s*[^,()]+)\s*\)\s*\./g;
-        var match = true;
-        var index = 0;
-        while(match = regex_arc.exec(code)){index = match.index + match[0].length};
-    
-        // Append the new member to TextArea
-        textarea.value = [textarea.value.slice(0,index), 'arc('+member1+','+member2+').', textarea.value.slice(index+1)].join('\n');
-        
-        // Updating TextArea by triggering the event "change"
-        $("#"+idTextArea).trigger('change');
-    }
-    
-    var deleteArc = function(member1,member2,idTextArea)
-    {
-        // Load TextArea
-        var textarea = document.getElementById(idTextArea);
-        
-        // Delete arcs, top and bottom predicates
-        var code = $("#"+idTextArea).val();
-        var regex_arc = new RegExp("arc\\s*\\(\\s*("+member1+"\\s*,\\s*"+member2+")\|("+member2+"\\s*,\\s*"+member1+")\\s*\\)\\s*\\.","g");
-        console.log(code.match(regex_arc));
-        code = code.replace(regex_arc,'\r');
-        
-        // Update Text and Graph
-        textarea.value = code;
-        $("#"+idTextArea).trigger('change');
-    }
 }
